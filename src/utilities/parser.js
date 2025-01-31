6bbeae1993a55ebe930d3d39aab3bd948aaed9d3
unknown <muhammadkhezar36@gmail.com>
2025-01-31 12:00:00 +0500

    added new helper functions

---
+++ src/utilities/parser.js
@@ -0,0 +1,52 @@
+export const makeFirtLetterCapital2 = (word) => {
+  if (!isUndefined(word)) {
+    return word?.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  }
+};
+
+export const IntoComma = (amount) => {
+  return amount.toLocaleString();
+};
+export const settabsActive = (tabs, tabName) => {
+  const ptabs = tabs.map((tab) => {
+    if (tab.title === tabName) {
+      return {
+        ...tab,
+        isActive: true,
+      };
+    } else
+      return {
+        ...tab,
+        isActive: false,
+      };
+  });
+
+  return ptabs;
+};
+
+export const parseMenusOnMountData = (menus, pathName) => {
+  let pMenus;
+  if (pathName !== "/") {
+    pMenus = menus.map((men) => {
+      if (men.to !== "/" && pathName?.includes(men.to)) {
+        return {
+          ...men,
+          isActive: true,
+        };
+      } else
+        return {
+          ...men,
+          isActive: false,
+        };
+    });
+  } else {
+    pMenus = [
+      { ...menus[0], isActive: true },
+      ...menus.slice(1).map((men) => {
+        return { ...men, isActive: false };
+      }),
+    ];
+  }
+
+  return pMenus;
+};
